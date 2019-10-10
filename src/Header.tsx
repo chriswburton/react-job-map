06646df71bcf5cf1d104d5b1d74d6e448f39e7f0
Chris Burton <chrisburton@Chriss-MacBook-Pro.local>
2019-10-10 20:19:54 +0100

    Further configuration of Emotion Styled Components with TailwindCSS - Trailed out using `react-app-rewired` + `customize-cra` instead of using Eject - Not awesome...

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import styled from '@emotion/styled';
+import tw from 'tailwind.macro';
 
-const Header: React.FC = () => <h1>Header</h1>;
+const HeaderWrapper = styled.div`
+    ${tw`py-4 text-4xl font-bold`};
+`;
+
+const Header: React.FC = () => <HeaderWrapper>Header</HeaderWrapper>;
 
 export default Header;
